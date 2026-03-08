76f89e2785e3b1fc8355cb3b6f93fe2e21f2f93d
Azam Hussain <azamdrive143@gmail.com>
2026-03-09 02:22:57 +0530

    feat: restrict invite code characters to uppercase letters and digits

--- server/src/utils/inviteCode.js
+++ server/src/utils/inviteCode.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export function generateInviteCode(length = 6) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   const bytes = crypto.randomBytes(length);
   let code = '';
   for (let i = 0; i < length; i++) {
